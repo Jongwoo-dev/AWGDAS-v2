8f5f5d1c7f043a51f9a68ae7705aa743a22192f5
jongwoo-dev <jongwoo.dev@gmail.com>
2026-04-14 23:28:27 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/jongwoo_dev/awgdas_v2/AwgdasV2Application.java
@@ -0,0 +1,13 @@
+package com.jongwoo_dev.awgdas_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwgdasV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwgdasV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongwoo_dev/awgdas_v2/AwgdasV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongwoo_dev.awgdas_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwgdasV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
